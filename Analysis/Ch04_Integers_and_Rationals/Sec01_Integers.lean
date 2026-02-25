ecd2f9e6afa66793b9cbbca96497875b8b31de26
BrandenXia <xxtbranden@outlook.com>
2026-02-25 07:58:23 -0800

    complete integer

--- Analysis/Ch04_Integers_and_Rationals/Sec01_Integers.lean
+++ Analysis/Ch04_Integers_and_Rationals/Sec01_Integers.lean
@@ -257,4 +257,201 @@ theorem Integer.sub_eq_formal_sub (a b : ℕ) : (a : Integer) - (b : Integer) = 
   simp only [Integer.sub_eq, Integer.natCast_eq, Integer.neg_eq, Integer.add_eq, Integer.eq]
   abel
 
+theorem Integer.mul_eq_zero {A B : Integer} (h : A * B = 0) : A = 0 ∨ B = 0 := by
+  obtain ⟨a, b⟩ := A
+  obtain ⟨c, d⟩ := B
+  change (a —— b) * (c —— d) = 0 —— 0 at h
+  simp only [mul_eq, eq, add_zero, zero_add] at h
+  change (a —— b) = 0 —— 0 ∨ (c —— d) = 0 —— 0
+  simp only [Integer.eq, add_zero, zero_add]
+  by_cases h': a = b
+  · left; exact h'
+  · right
+    rcases Nat.lt_trichotomy a b with (ha | hab | hb)
+    · obtain ⟨n, rfl⟩ := Nat.exists_eq_add_of_lt ha
+      rw [add_assoc a] at h
+      conv at h =>
+        left
+        rw [add_mul, <-add_assoc]
+      conv at h =>
+        right
+        rw [add_mul, <-add_assoc]
+        left
+        rw [add_comm]
+      simp only [add_left_cancel_iff] at h
+      let n_plus_one_neq_zero : n + 1 > 0 := by simp
+      rw [Nat.mul_left_cancel_iff n_plus_one_neq_zero] at h
+      symm at h
+      exact h
+    · contradiction
+    · obtain ⟨n, rfl⟩ := Nat.exists_eq_add_of_lt hb
+      rw [add_assoc b] at h
+      conv at h =>
+        left
+        rw [add_mul, add_assoc]
+        right
+        rw [add_comm]
+      conv at h =>
+        left
+        rw [<-add_assoc]
+      conv at h =>
+        right
+        rw [add_mul, add_assoc]
+        right
+        rw [add_comm]
+      conv at h =>
+        right
+        rw [<-add_assoc]
+        left
+        rw [add_comm]
+      simp only [add_left_cancel_iff] at h
+      let n_plus_one_neq_zero : n + 1 > 0 := by simp
+      rw [Nat.mul_left_cancel_iff n_plus_one_neq_zero] at h
+      exact h
+
+theorem Integer.mul_right_cancel (a b c : Integer) (h : a * c = b * c) (hc : c ≠ 0) : a = b := by
+  apply_fun (· - b * c) at h
+  simp only [sub_self] at h
+  rw [<-mul_sub_right_distrib] at h
+  apply Integer.mul_eq_zero at h
+  cases h with
+  | inl hp => simp only [sub_eq_iff_eq_add, zero_add] at hp; exact hp
+  | inr hq => contradiction
+
+instance : LE Integer where
+  le x y := ∃ n : ℕ, y = x + n
+
+instance : LT Integer where
+  lt x y := x ≤ y ∧ x ≠ y
+
+theorem Integer.lt_iff_exists_pos_diff (a b : Integer) : a > b ↔ ∃ n : ℕ, n ≠ 0 ∧ a = b + n := by
+  constructor
+  · rintro ⟨h_lt, h_neq⟩
+    obtain ⟨n, rfl⟩ := h_lt
+    exists n
+    constructor
+    · simp only [ne_eq, left_eq_add] at h_neq
+      change ¬(n —— 0) = 0 —— 0 at h_neq
+      simp only [eq, add_zero] at h_neq
+      exact h_neq
+    · rfl
+  · intro h
+    obtain ⟨n, hn, rfl⟩ := h
+    constructor
+    · exists n
+    · simp only [ne_eq, left_eq_add]
+      change ¬(n —— 0) = 0 —— 0
+      simp only [eq, add_zero]
+      exact hn
+
+theorem Integer.add_lt_add_right {a b : Integer} (c : Integer) (h : a < b) : a + c < b + c := by
+  let ⟨h_lt, h_neq⟩ := h
+  obtain ⟨n, rfl⟩ := h_lt
+  simp only [lt_iff_exists_pos_diff]
+  exists n
+  constructor
+  · simp only [ne_eq, left_eq_add] at h_neq
+    change ¬(n —— 0) = 0 —— 0 at h_neq
+    simp only [eq, add_zero] at h_neq
+    exact h_neq
+  · group
+
+theorem Integer.mul_lt_mul_pos_right {a b c : Integer} (h : a < b) (hc : c > 0)
+    : a * c < b * c := by
+  simp only [lt_iff_exists_pos_diff] at *
+  obtain ⟨n, h_n_nzero, rfl⟩ := h
+  obtain ⟨c', h_cn0, rfl⟩ := hc
+  exists n * c'
+  constructor
+  · exact Nat.mul_ne_zero h_n_nzero h_cn0
+  · simp only [zero_add, Nat.cast_mul]
+    group
+
+theorem Integer.neg_lt_neg {a b : Integer} (h : a < b) : -a > -b := by
+  simp only [lt_iff_exists_pos_diff] at *
+  obtain ⟨n, h_n_nzero, rfl⟩ := h
+  exists n
+  constructor
+  · exact h_n_nzero
+  · group
+
+theorem Integer.neg_le_neg {a b : Integer} (h : a ≤ b) : -a ≥ -b := by
+  obtain ⟨n, rfl⟩ := h
+  exists n
+  group
+
+theorem Integer.lt_trans {a b c : Integer} (hab : a < b) (hbc : b < c) : a < c := by
+  simp only [lt_iff_exists_pos_diff] at *
+  obtain ⟨n₁, h_n₁_nzero, rfl⟩ := hab
+  obtain ⟨n₂, h_n₂_nzero, rfl⟩ := hbc
+  exists n₁ + n₂
+  constructor
+  · exact Iff.mpr add_ne_zero <| Or.inl h_n₁_nzero
+  · simp; group
+
+theorem Integer.trichotomous (a b : Integer) : one_hot (a < b) (a > b) (a = b) := by
+  rcases Integer.trichotomy (a - b) with (h_pos | h_neg | h_zero)
+  · right; left
+    let ⟨h_pos, h_nneg, h_neq⟩ := h_pos
+    simp only [sub_eq_iff_eq_add, zero_add, <-ne_eq] at h_neq
+    and_intros
+    · by_contra
+      simp only [lt_iff_exists_pos_diff] at this
+      obtain ⟨n, hn, rfl⟩ := this
+      obtain ⟨m, hm, h_meq⟩ := h_pos
+      simp only [sub_add_cancel_left] at h_meq
+      change 0 —— n = m —— 0 at h_meq
+      rw [eq, add_zero] at h_meq
+      symm at h_meq
+      simp only [Nat.add_eq_zero_iff] at h_meq
+      let n_eq0 := And.right h_meq
+      contradiction
+    · obtain ⟨n, h_ngt0, h_neq⟩ := h_pos
+      exists n
+      simp only [sub_eq_iff_eq_add, add_comm] at h_neq
+      exact h_neq
+    · symm at h_neq
+      exact h_neq
+    · exact h_neq
+  · left
+    let ⟨h_npos, h_neg, h_neq⟩ := h_neg
+    simp only [sub_eq_iff_eq_add, zero_add, <-ne_eq] at h_neq
+    and_intros
+    · obtain ⟨n, hn, h_neq⟩ := h_neg
+      exists n
+      rw [sub_eq_iff_eq_add, add_comm] at h_neq
+      symm at h_neq
+      rw [<-Integer.sub_eq, sub_eq_iff_eq_add] at h_neq
+      exact h_neq
+    · exact h_neq
+    · by_contra
+      simp only [lt_iff_exists_pos_diff] at this
+      obtain ⟨n, hn, rfl⟩ := this
+      obtain ⟨m, hm, h_meq⟩ := h_neg
+      simp only [add_sub_cancel_left] at h_meq
+      change n —— 0 = 0 —— m at h_meq
+      simp only [eq, add_zero, add_eq_zero] at h_meq
+      let n_eq0 := And.left h_meq
+      contradiction
+    · exact h_neq
+  · right; right
+    obtain ⟨h_npos, h_nneg, h_zero⟩ := h_zero
+    simp only [sub_eq_iff_eq_add, zero_add] at h_zero
+    and_intros
+    · by_contra
+      simp only [lt_iff_exists_pos_diff] at this
+      obtain ⟨n, hn, rfl⟩ := this
+      simp only [left_eq_add] at h_zero
+      change n —— 0 = 0 —— 0 at h_zero
+      simp only [eq, add_zero] at h_zero
+      contradiction
+    · by_contra
+      simp only [lt_iff_exists_pos_diff] at this
+      obtain ⟨n, hn, rfl⟩ := this
+      simp only [add_eq_left] at h_zero
+      change n —— 0 = 0 —— 0 at h_zero
+      simp only [eq, add_zero] at h_zero
+      contradiction
+    · exact h_zero
+
 end Analysis.Ch04.Sec01
